8b83ad8a2c52c988d16285ac1d25eadecaf44e64
Excelsio <vapeexcelsio@gmail.com>
2025-04-07 22:31:34 +0300

    Добавление секций "Галерея" , обновление зависимостей UIkit до версии 3.23.5, улучшение стилей и адаптивности, а также исправление ошибок в анимации. Обновлены стили для различных элементов и добавлены новые переменные в SCSS.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
+// import "/node_modules/uikit/dist/css/uikit-core.min.css";
 
-import "/node_modules/uikit/dist/css/uikit-core.min.css"
-// import "/node_modules/uikit/dist/js/uikit-core.min.js";
-// import 'node_modules/bootstrap/dist/css/bootstrap.min.css';
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+import "uikit/dist/css/uikit.min.css";
 import "/src/sass/style.scss";
+
+// подключаем иконки (иначе стрелки в слайдере не появятся)
+UIkit.use(Icons);
